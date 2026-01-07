4ba0c72e66e8fa646e8616c79cf26555a22bdb98
Ari Sobel <arieh.sobel@gmail.com>
2026-01-07 00:52:37 +0000

    add  and  files

--- pages/api/v1/status.js
+++
@@ -1,5 +0,0 @@
-function status(request, response) {
-  response.status(200).json({ chave: "são média" });
-}
-
-export default status;
---
+++ pages/api/v1/status/index.js
@@ -0,0 +1,10 @@
+import "../../../../infra/database.js";
+import database from "../../../../infra/database.js";
+
+async function status(request, response) {
+  const result = await database.query("SELECT 1+1 as sum;");
+  console.log(result.rows);
+  response.status(200).json({ chave: "são média" });
+}
+
+export default status;
