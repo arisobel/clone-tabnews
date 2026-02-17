7fb7a278e4153daffbf678286902fc4b13024626
Ari Sobel <arieh.sobel@gmail.com>
2026-02-17 14:04:43 +0000

    BH - refactor: improve database query handling and enhance status API response

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,23 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1+1 as sum;");
-  console.log(result.rows);
-  response.status(200).json({ chave: "são média" });
+  const updatedAt = new Date().toISOString();
+  const dbVersion = await database.query({text:"SHOW server_version;"});
+  const maxConn  = await database.query({text:"SHOW max_connections;"});
+  const oppened_conections  = await database.query({text:"SELECT * FROM pg_stat_activity where datname = 'local_db';"});
+  
+  response.status(200).json({
+    updated_at: updatedAt,
+    
+    dependencies:{
+      database: {
+        ver: dbVersion.rows[0].server_version,
+        max_conns: parseInt(maxConn.rows[0].max_connections),
+        oppened_conections: oppened_conections.rows.length
+      },
+    },
+    
+  });
 }
 
 export default status;
--- testes/integration/api/v1/status/get.test.js
+++ testes/integration/api/v1/status/get.test.js
@@ -1,4 +1,36 @@
 test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+  expect(responseBody.updated_at).toBeDefined();
+  expect(responseBody.dependencies.database.ver).toBeDefined();
+  expect(responseBody.dependencies.database.oppened_conections).toBeDefined();
+  expect(responseBody.dependencies.database.max_conns).toBeDefined();
+  
+  new Date(responseBody.updated_at).toISOString();
+  
+  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString()
+  expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
+  
+  
+  
+  //verificando tipo de dados e ddados de ver (versao)
+  const dbVer = responseBody.dependencies.database.ver;
+  expect(typeof dbVer).toBe("string");
+  expect(dbVer).toBe("16.0");
+  
+  //verificando tipo de dados e dados de max_cons (quantidade de conexoes usadas)
+  const dbConn = parseInt(responseBody.dependencies.database.oppened_conections, 10);
+  //expect(typeof dbConn).toBe("number");
+  expect(dbConn).toBe(1);
+  
+  //verificando tipo de dados e dados de conections (quantidade de conexoes possiveis)
+  const dbMaxCons = responseBody.dependencies.database.max_conns
+  //expect(typeof dbMaxCons).toBe("number");
+  expect(dbMaxCons).toBe(100);
+
+  const dbOpenCons = responseBody.dependencies.database.oppened_conections
+  expect(dbOpenCons).toEqual(1);
+
 });
